9ee214447821072b0ffe86969da40742869c0785
seo-young-kim <seoykim996@gmail.com>
2021-04-22 07:44:41 +0000

    recover

---
+++ main/__init__.py
@@ -0,0 +1,36 @@
+from flask import Flask,request,render_template
+import sys
+sys.path.append('/app/main')
+
+from script.logger import logger
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/index4')
+def home_seoul():
+    return render_template('index4.html')
+
+try:
+    value=predict()
+except :
+    value ={}
+    logger.error("fail call fct : predict()")
+# 서영
+@app.route('/predict_castle')
+def castle_predict():
+    try:
+        return render_template('predict_castle.html',data=value)
+    except :
+        return 'predict_castle.html'
+
+@app.route('/select.html')
+def select():
+    return render_template('select.html')
+
+
